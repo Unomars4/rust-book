d5da41aef4e892553f9067b64b460c908ce928d0
Une Nyide <une.nyide@gmail.com>
2024-10-29 21:23:09 +0200

    add release build for hello_cargo

---
+++ hello_cargo/target/release/deps/hello_cargo-20f4f9484bcfacdd.d
@@ -0,0 +1,5 @@
+/Users/Unomars/projects/rust-book/hello_cargo/target/release/deps/hello_cargo-20f4f9484bcfacdd: src/main.rs
+
+/Users/Unomars/projects/rust-book/hello_cargo/target/release/deps/hello_cargo-20f4f9484bcfacdd.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/Unomars/projects/rust-book/hello_cargo/target/release/hello_cargo: /Users/Unomars/projects/rust-book/hello_cargo/src/main.rs
