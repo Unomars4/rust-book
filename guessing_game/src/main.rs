3c40b8d7a5f79f1aacc9c03a96941121d929da5a
Une Nyide <une.nyide@gmail.com>
2025-04-13 11:04:14 +0200

    complete guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,17 +1,33 @@
-use std::io;
+use rand::Rng;
+use std::{cmp::Ordering, io};
 
 fn main() {
-    println!("+++++++++++++++++++++++++++");
     println!("Guess the number!");
-    println!("+++++++++++++++++++++++++++");
 
-    println!("Please input your guess");
+    println!("Please input your guess:");
+    let answer = rand::thread_rng().gen_range(1..=100);
+    loop {
+        let mut users_guess = String::new();
 
-    let mut users_guess = String::new();
+        io::stdin()
+            .read_line(&mut users_guess)
+            .expect("Failed to read the guess");
 
-    io::stdin()
-        .read_line(&mut users_guess)
-        .expect("Failed to read the guess");
+        let users_guess: u32 = match users_guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("Your guess {}", users_guess);
+        println!("Your guess {}", users_guess);
+
+        match users_guess.cmp(&answer) {
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+            Ordering::Greater => println!("Too high"),
+            Ordering::Less => println!("Too low"),
+        }
+        println!("Try guessing again:")
+    }
 }
